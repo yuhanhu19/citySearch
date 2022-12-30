a089aed5132a54da5fd2dc46ded8bb07e9755286
Yuhan Hu <yuhan.hu@cat.com>
2022-12-31 01:20:39 +0800

    [Yuhan] feat: implement throw exception given null text

--- citySearch/citySearch/CitySearch.cs
+++ citySearch/citySearch/CitySearch.cs
@@ -1,11 +1,23 @@
+using System;
 using System.Collections.Generic;
 
 namespace citySearch
 {
     public class CitySearch
     {
-        public string Search(string cityname)
+        IList<string> cityNames = new List<string>
         {
+            "Paris", "Budapest", "Skopje", "Rotterdam", "Valencia", "Vancouver", "Amsterdam", "Vienna", "Sydney",
+            "New York City", "London", "Bangkok", "Hong Kong", "Dubai", "Rome", "Istanbul"
+        };
+
+        public string[] Search(string text)
+        {
+            if (string.IsNullOrEmpty(text))
+            {
+                throw new Exception("Search text must be provided");
+            }
+
             return null;
         }
     }
